91eb13916cb893821b6490e52fd6c989592e6e82
root <root@localhost.localdomain>
2018-11-25 15:19:24 +0100

    add script

--- BURLE_Clelia_kholle_1.py
+++ BURLE_Clelia_kholle_1.py
@@ -21,7 +21,10 @@ parser.add_argument("-c", action="store_true", help="Supprime tous les élément
 parser.add_argument("-max", action="store_true", help="Affiche la valeur maximum contenu dans la liste") 
 parser.add_argument("-min", action="store_true", help="Affiche la valeur minimal contenu dans la liste") 
 #parser.add_argument("-moy", action="store_true", help="Affiche la moyenne de tous les éléments dans la liste") 
-parser.add_argument("-sum", action="store_true", help="Affiche la somme de tous les éléments dans la liste") 
+#parser.add_argument("-sum", action="store_true", help="Affiche la somme de tous les éléments dans la liste") 
+parser.add_argument("-t", action="store_true", help="Trie la liste dans l'ordre croissant") 
+parser.add_argument("-desc", action="store_true", help="Trie la liste dans l'odre décroissant") 
+parser.add_argument("-help", action="store_true", help="Affiche le manuel d'utilisation") 
 
 args = parser.parse_args()
  
@@ -73,17 +76,27 @@ elif args.min:
 
         print("la valeur minimal est : ", mini)
        
-#elif args.moy
+elif args.t: 
 
-
-elif args.sum:
-
-    with open ('list.csv', 'rt') as csv_file:
+    with open('list.csv', 'r') as csv_file: 
         csv_reader = csv.reader(csv_file) 
 
         for line in csv_reader: 
-            summ=sum(line) 
+            sorted(line)
 
-        print("la somme est de : ", summ)  
+        
+elif args.desc: 
+
+    with open('list.csv', 'r') as csv_file: 
+        csv_reader = csv.reader(csv_file) 
+
+        for line in csv_reader: 
+            sorted(line, reverse=True) 
+
+elif args.help: 
+
+    print("-l : Affiche le contenu de la liste \n-a : Ajoute les items dans la liste \n-c :Supprime tous les éléments de la liste \n-max : Affiche la valeur maximum contenu dans la liste \n-min : Affiche la valeur minimum dans la liste \n-moy : Affiche la moyenne de tous les éléments dans la liste \n-sum : Affiche la somme de tous les éléments dans la liste \n-t  : Trie la liste dans l'ordre croissant\n-desc : Trie la liste dans l'ordre décroissant") 
+             
+
 
 
