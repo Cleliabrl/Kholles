2c58331b25229db06617936945b2174181f62a65
root <root@localhost.localdomain>
2018-11-25 14:43:06 +0100

    min

--- BURLE_Clelia_kholle_1.py
+++ BURLE_Clelia_kholle_1.py
@@ -10,15 +10,18 @@ import csv
 
 #VARIABLES 
 li = []
-minNbr, maxNbr = [],[]
-
+mini=0
+maxi=0
+summ=0
 #ARGUMENTS
 parser = argparse.ArgumentParser() 
 parser.add_argument("-l", action="store_true", help="Affiche le contenu de la liste")
 parser.add_argument("-a", type=int, nargs='+', help="Ajoute les items dans la liste") 
 parser.add_argument("-c", action="store_true", help="Supprime tous les éléments de la liste")
-parser.add_argument("-s", "--max", action="store_true", help="Affiche la valeur maximum contenu dans la liste") 
-parser.add_argument("-s", "--min", action="store_true", help="Affiche la valeur minimal contenu dans la liste") 
+parser.add_argument("-max", action="store_true", help="Affiche la valeur maximum contenu dans la liste") 
+parser.add_argument("-min", action="store_true", help="Affiche la valeur minimal contenu dans la liste") 
+#parser.add_argument("-moy", action="store_true", help="Affiche la moyenne de tous les éléments dans la liste") 
+parser.add_argument("-sum", action="store_true", help="Affiche la somme de tous les éléments dans la liste") 
 
 args = parser.parse_args()
  
@@ -57,17 +60,30 @@ elif args.max:
         for line in csv_reader: 
             maxi=max(line) 
 
-        print("max value is : ", maxi ) 
+        print("la valeur maximal est : ", maxi ) 
 
+   
 elif args.min: 
 
     with open('list.csv', 'rt') as csv_file: 
-        csv_reader = csv.reader(csv_file)
+        csv_reader = csv.reader(csv_file) 
 
         for line in csv_reader: 
             mini=min(line) 
 
-        print("min value is : ", mini ) 
+        print("la valeur minimal est : ", mini)
+       
+#elif args.moy
 
 
- 
+elif args.sum:
+
+    with open ('list.csv', 'rt') as csv_file:
+        csv_reader = csv.reader(csv_file) 
+
+        for line in csv_reader: 
+            summ=sum(line) 
+
+        print("la somme est de : ", summ)  
+
+
